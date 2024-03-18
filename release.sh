3f0ec8f5f46e572cc8fe3c6039e5b3b48c098881
Yannick Poirier <yannick.poirier@ses.com>
2024-03-18 08:27:01 +0100

    Script to create a release

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ fi
 
 cargo changelog --write flute || exit -1
 cargo set-version $version  || exit -1
-git commit Cargo.toml CHANGELOG.md -m "release-$version"
-git tag release-$version
+git commit Cargo.toml CHANGELOG.md -m "v$version"
+git tag v$version
 git push
 git push --tags
