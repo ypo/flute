c28332dd69976d0386ed4de06b3f5137bca770cb
Yannick Poirier <yannick.poirier@ses.com>
2024-09-04 09:14:46 +0200

    Remove cargo changelog

--- release.sh
+++ release.sh
@@ -9,9 +9,8 @@ else
 version=$1
 fi
 
-cargo changelog --write flute || exit -1
 cargo set-version $version  || exit -1
-git commit Cargo.toml CHANGELOG.md -m "v$version"
+git commit Cargo.toml -m "v$version"
 git tag v$version
 git push
 git push --tags
